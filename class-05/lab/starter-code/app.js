f70d500d1e7016bbe74666b6df15ad09e997bbf8
will-ing <willkoger@gmail.com>
2020-02-07 14:06:48 -0800

    multi complete

--- class-05/lab/starter-code/app.js
+++ class-05/lab/starter-code/app.js
@@ -11,12 +11,12 @@ Test this function by hand in the console to get it working, and when you think 
 function sum(a, b) { //eslint-disable-line
   var add = a + b;
   var str = 'The sum of ' + a + ' and ' + b + ' is 11.'
-  var arr =[add, str]
-  return console.log(arr)
+  var arr = [add, str]
+  return(arr);
 }
-sum(4,7);
+
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -30,11 +30,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
-
+  var mult = a * b;
+  var times = 'The product of ' + a + ' and ' + b + ' is ' + mult + '.'
+  var arrtwo =[mult, times];
+  return(arrtwo);
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
