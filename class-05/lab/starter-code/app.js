dedb1c219fbf701eb8e8551aaa0f50811ed214ac
Jesse Pena <jpchato@gmail.com>
2020-02-07 16:50:58 -0800

    problem 3 solved

--- class-05/lab/starter-code/app.js
+++ class-05/lab/starter-code/app.js
@@ -59,11 +59,22 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+  var sumAndMultiplyArr = [16, 140, a + ' and ' + b + ' and ' + c + ' sum to 16.', 'The product of ' + a + ' and ' + b + ' and ' + c + ' is 140.'];
+  var helpMe = sum (a, b)[0];
+  var newSum = sum(helpMe, c)[0];
+  console.log(newSum);
+  var pleaseHelpMe = multiply (a, b)[0];
+  var newMultiply = multiply (pleaseHelpMe, c)[0];
+  console.log(newMultiply);
+
+  return sumAndMultiplyArr;
+  // console.log(sumAndMultiplyArr);
+  
 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
